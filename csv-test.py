f91b130eb17d1777d882d9f84dcf3aa795916e87
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-06-20 11:44:11 +0000

    Fixed appending and writing to csv file

--- csv-test.py
+++ csv-test.py
@@ -1,39 +1,40 @@
 import csv
 from os.path import exists
 
-header = ['first_name', 'last_name', 'telephone', 'town']
-file_exists = exists('data.csv')
-if file_exists is False:
-    print("\n\033[1mCreating file\033[0m")
-    with open('data.csv', 'w+', encoding='utf8') as file:
-        csvwriter = csv.writer(file)
-        csvwriter.writerow(header)
-        file.close()
-
-
-array = []
 data_to_write = []
 
 while True:
+    temp_array = []
     first_name = input("Enter the customer's first name: ").title()
     last_name = input("Enter the customer's last name: ").title()
     telephone = input("Enter the customer's phone number: ")
     town = input("Enter the customer's town: ").title()
-    
-    data_to_write.append(first_name)
-    data_to_write.append(last_name)
-    data_to_write.append(telephone)
-    data_to_write.append(town)
-    array.append(data_to_write)
+    temp_array.append(first_name)
+    temp_array.append(last_name)
+    temp_array.append(telephone)
+    temp_array.append(town)
+    print(temp_array)
+    data_to_write.append(temp_array)
+    print(f"Data to write: {data_to_write}")
     to_continue = input("\nDo you want to continue? (y/n): ").title()
     if to_continue == "N":
         break
 
-with open('data.csv', 'r', encoding='utf-8') as read_file:
-    content = read_file.readlines()
-read_header = content[:1]
-read_rows = content[1:]
+# with open('data.csv', 'r', encoding='utf-8') as read_file:
+#     content = read_file.readlines()
+# read_header = content[:1]
+# read_rows = content[1:]
 
-with open('data.csv', 'w+', newline="") as writing_file:
-    csvwriter = csv.writer(writing_file) # 2. create a csvwriter object
-    csvwriter.writerows(array) # 5. write the rest of the data
+file_exists = exists('data.csv')
+if file_exists is False:
+    header = ['first_name', 'last_name', 'telephone', 'town']
+    with open('data.csv', 'w+', newline="", encoding='utf-8') as writing_file:
+        csvwriter1 = csv.writer(writing_file) # 2. create a csvwriter object
+        csvwriter1.writerow(header) # 4. write the header
+        csvwriter1.writerows(data_to_write) # 5. write the rest of the data
+        writing_file.close()
+else:
+    with open('data.csv', 'a', newline="", encoding='utf-8') as appending_file:
+        csvwriter2 = csv.writer(appending_file)
+        csvwriter2.writerows(data_to_write)
+        appending_file.close()
