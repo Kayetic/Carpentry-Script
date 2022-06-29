5908e892d2ff26da7f05ca8a845ecf2523b70053
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-06-29 18:54:11 +0000

    Fixed csv writer splitting every string with comma

--- external_modules.py
+++ external_modules.py
@@ -52,12 +52,12 @@ def writing_data_csv(filename, data_to_write):
         with open(filename, 'w+', newline="", encoding='utf-8') as writing_file:
             csvwriter1 = csv.writer(writing_file) # 1. create a csvwriter object
             csvwriter1.writerow(header) # 2. write the header
-            csvwriter1.writerows(data_to_write) # 3. write the rest of the data
+            csvwriter1.writerow(data_to_write) # 3. write the rest of the data
             writing_file.close() # 4. close the file
     else:
         with open(filename, 'a', newline="", encoding='utf-8') as appending_file:
             csvwriter2 = csv.writer(appending_file) # 1. create a csvwriter object
-            csvwriter2.writerows(data_to_write) # 2. write the rows, without the header
+            csvwriter2.writerow(data_to_write) # 2. write the row, without the header
             appending_file.close() # 3. close the file
 
 def reading_data_csv(filename):
